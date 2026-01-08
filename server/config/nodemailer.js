ad9d42cd3a32b9e19f886fac911ef7af7ff697bc
= <=>
2026-01-08 12:54:41 +0530

    nodemailer

--- server/config/nodemailer.js
+++ server/config/nodemailer.js
@@ -1,12 +1,17 @@
 const nodemailer = require("nodemailer");
 require("dotenv").config();
 
-
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false, // 587 ke liye hamesha false rahega
     auth: {
         user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS,
+        pass: process.env.SMTP_PASS, // Yahan 16-digit App Password hi hona chahiye
+    },
+    tls: {
+        // Render network par SSL issues se bachne ke liye
+        rejectUnauthorized: false 
     }
 });
 
